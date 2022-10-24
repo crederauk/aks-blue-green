002c730656f68e718f80e60b9857561f6a16bf9f
MattToczek <toczek.matt@gmail.com>
2022-10-24 16:51:35 +0100

    uncomment kubectl provider

--- terraform/aks/green/providers.tf
+++ terraform/aks/green/providers.tf
@@ -45,9 +45,9 @@ provider "helm" {
   }
 }
 
-# provider "kubernetes" {
-#   host                   = azurerm_kubernetes_cluster.green_cluster.kube_admin_config.0.host
-#   client_certificate     = base64decode(azurerm_kubernetes_cluster.green_cluster.kube_admin_config.0.client_certificate)
-#   client_key             = base64decode(azurerm_kubernetes_cluster.green_cluster.kube_admin_config.0.client_key)
-#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.green_cluster.kube_admin_config.0.cluster_ca_certificate)
-# }
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.green_cluster.kube_admin_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.green_cluster.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.green_cluster.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.green_cluster.kube_admin_config.0.cluster_ca_certificate)
+}
